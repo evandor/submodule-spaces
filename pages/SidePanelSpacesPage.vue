1df6eec765b0aa77c1f92383f99e31c86efe27d9
Carsten Gräf <evandor@gmail.com>
2024-11-10 11:24:31 +0100

    merging manually

--- pages/SidePanelSpacesPage.vue
+++ pages/SidePanelSpacesPage.vue
@@ -148,7 +148,7 @@ import NavigationService from "src/services/NavigationService";
 import NewTabsetDialog from "src/tabsets/dialogues/NewTabsetDialog.vue";
 import {useUiStore} from "src/ui/stores/uiStore";
 import InfoMessageWidget from "src/ui/widgets/InfoMessageWidget.vue";
-import FirstToolbarHelper from "pages/sidepanel/helper/FirstToolbarHelper.vue";
+import FirstToolbarHelper from "pages/sidepanel/helper/FirstToolbarHelper2.vue";
 import {useWindowsStore} from "src/windows/stores/windowsStore";
 import Analytics from "src/core/utils/google-analytics";
 import {Space} from "src/spaces/models/Space";
