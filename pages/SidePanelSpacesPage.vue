ba3cc1390e1515f710b8f68aefd1060ede27d508
Carsten Gräf <evandor@gmail.com>
2024-11-21 13:46:27 +0100

    tabsets-289

--- pages/SidePanelSpacesPage.vue
+++ pages/SidePanelSpacesPage.vue
@@ -157,7 +157,6 @@ import {useTabsetService} from "src/tabsets/services/TabsetService2";
 import {useTabsStore2} from "src/tabsets/stores/tabsStore2";
 import {useTabsetsStore} from "src/tabsets/stores/tabsetsStore";
 import {useFeaturesStore} from "src/features/stores/featuresStore";
-import {SidePanelViews} from "src/app/models/SidePanelViews";
 import NavTabsetsListWidgetNonBex from "src/tabsets/widgets/NavTabsetsListWidgetNonBex.vue";
 
 const {inBexMode} = useUtils()
